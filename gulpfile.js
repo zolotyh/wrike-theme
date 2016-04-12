3d2d542abacadcde6533fd2f4ace2f162a277c29
zolotyh <alexey.zolotykh@team.wrike.com>
2016-04-12 23:56:59 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+const autoprefixer = require('autoprefixer');
+const csso = require('postcss-csso');
+const gulp = require('gulp');
+const header = require('gulp-header');
+const postcss = require('gulp-postcss');
+const sass = require('gulp-sass');
+const sync = require('browser-sync').create();
+
+// Banner
+
+const pkg = require('./package.json');
+const banner = `/**
+ * ${ pkg.description }
+ * ${ pkg.name } v${ pkg.version }, ${ pkg.homepage }
+ * @copyright 2010–${ new Date().getFullYear() } ${ pkg.author.name }, ${ pkg.author.url }
+ * @license ${ pkg.license }
+ */
+`;
+
+// Server
+
+gulp.task('default', ['styles'], () => {
+	sync.init({
+		ui: false,
+		notify: false,
+		server: {
+			baseDir: '.',
+			routes: {
+				'/shower-core': '../shower-core'
+			}
+		}
+	});
+
+	gulp.watch('styles/**/*.scss', ['styles']);
+	gulp.watch('index.html').on('change', sync.reload);
+});
+
+// Styles
+
+gulp.task('styles', () => {
+	return gulp.src('styles/screen-*.scss')
+		.pipe(sass().on('error', sass.logError))
+        .pipe(postcss([
+			autoprefixer,
+			csso
+		]))
+		.pipe(header(banner, { pkg: pkg }))
+		.pipe(gulp.dest('styles'))
+		.pipe(sync.stream());
+});
